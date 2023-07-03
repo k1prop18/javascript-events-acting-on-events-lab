f15a3572e9f986f0c7c9b81ae73d1e19fd31c052
Caleb Kiprop Rono <ckiprop14@gmail.com>
2023-07-03 11:47:08 +0300

    Lab done

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+dodger.addEventListener('keydown', (event)=>{
+    if (event.key === "ArrowLeft"){
+        const leftNumbers = dodger.style.left.replace('px', "")
+        const left = parseInt(leftNumbers, 10)
+
+        dodger.style.left = `${left - 1}px`
+    }
+})
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  
+
+  
+  
+    
+
+}
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+    
+    
+    }
+    
+
+    
+  
+
+
+    
+  
+
+
+
+  
+
+
+    
